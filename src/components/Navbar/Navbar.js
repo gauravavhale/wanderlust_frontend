161a6c4c001ac509a5faad5292eff08594259439
gaurav avhale <avhalegaurav07@gmail.com>
2025-07-30 22:38:56 +0530

    Favicon added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const Navbar = () => {
 
@@ -8,17 +9,18 @@ export const Navbar = () => {
         { href:'/destinantion', label:'Destinations' },
         { href:'/packages', label:'Packages '},
         { href:'/blog', label:'Blog'},
-        { href:'/contact', label:'Contact'},
         { href:'/login', label:'Login'},
     ]
 
   return (
-    <nav className="py-4 px-8 top-16 left-0 w-full z-1000 bg-[#F7F7F7]">
+    <nav className="py-1 px-8 top-16 left-0 w-full z-1000 bg-[#F7F7F7]">
         <div className="max-w-7xl mx-auto flex items-center justify-between">
     
-        <div className="text-2xl font-bold text-purple-950">WanderLust Travel Hub</div>
+        <div className="text-2xl flex items-center ">
+          <Image src={'/images/wanderlust.png'} width={50} height={50} alt='Wanderlust'/>
+        </div>
 
-        <div className="hidden md:flex gap-6 text-lg font-medium text-purple-950">
+        <div className="flex gap-6 text-lg font-medium text-purple-950">
             {navLinks.map((link) => (
             <Link
             href={link.href}
